a14254dffb99270ee1d2a02b78f172f6573aaeb5
hwc9169 <hwc9169@gmail.com>
2022-01-25 17:17:03 +0900

    caching 및 commit 정렬 작업

--- app/main.py
+++ app/main.py
@@ -1,18 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import List
 
+from app.entity import Commit
+
+import json
 import aioredis
 
-app = FastAPI()
+from dotenv import load_dotenv
+load_dotenv()
+
 redis = aioredis.from_url('redis://redis')
+app = FastAPI()
 
-
-@app.get("/v1/commits/rank/{github_id}")
+@app.get("/v1/commits/rank/{github_id}", response_model=Commit)
 async def read_my_commits_rank(github_id: str):
-    commits_info = await redis.get(github_id)
+    commit = Commit(
+                github_id=github_id,
+                order_criteria=None,
+                **json.loads(await redis.get(github_id))
+            )
 
-    return commits_info
+    return commit
 
 
-@app.get("/v1/commits/rank")
-async def read_commits_rank():
-    pass
+@app.get("/v1/commits/rank", response_model=List[Commit])
+async def read_commits_rank(sort: str='week'):
+    github_ids = await redis.keys("*")
+    response: List[Commit] = []
+
+    if sort != 'week' and sort != 'total':
+        return HTTPException(status_code=400, detail='sort는 week나 total만 가능합니다.')
+
+    cache = await redis.get(f'sorted_{sort}_commits') # sorted_week_commits or sorted_total_commits
+    if not cache:
+        for github_id in github_ids:
+            commit = Commit(
+                github_id=github_id.decode(),
+                order_criteria=sort,
+                **json.loads(await redis.get(github_id))
+            )
+            response.append(commit)
+        response.sort()
+        print([r.__dict__ for r in response])
+        await redis.set(f'sorted_{sort}_commits', json.dumps([r.__dict__ for r in response]))
+
+    print(await redis.get(f'sorted_{sort}_commits'))
+    response = json.loads(await redis.get(f'sorted_{sort}_commits'))
+
+    return response
